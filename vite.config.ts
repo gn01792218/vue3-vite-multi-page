e45d36fd15339f6130b6d30f988b99af1c51e41c
jacky <jacky@metallurgietech.com>
2023-01-12 16:46:18 +0800

    修改

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { loadEnv } from 'vite'
 import { resolve } from 'path'
-const root = resolve(__dirname, 'src')
-const outDir = resolve(__dirname, 'dist')
+
 // https://vitejs.dev/config/
 export default ({ mode }) => {
   const env = loadEnv(mode, process.cwd())
@@ -19,16 +18,16 @@ export default ({ mode }) => {
         '@': resolve(__dirname, 'src')
       }
     },
-    root,
+    root:resolve(__dirname, 'src'),
     base: env.VITE_BASE_URL,
     build: {
-      outDir,
+      outDir:resolve(__dirname, 'dist'),
       emptyOutDir: true,
       rollupOptions: {
         input: {
           //第一個參數是根目錄，第二個參數是資料夾路徑，第三個參數是入口檔案
-          main: resolve(root, 'pages/main', 'index.html'),
-          template2: resolve(root, 'pages/template2/', 'index.html')
+          main: resolve(__dirname,'src/pages/main/index.html'),
+          template2: resolve(__dirname,'src/pages/template2/index.html')
         }
       }
     }
